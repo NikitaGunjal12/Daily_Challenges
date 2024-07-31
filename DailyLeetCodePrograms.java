32d5d59e59bb87437db90e891b51d04a192025ae
NikitaGunjal12 <nikitagunjal.rmdstic.comp@gmail.com>
2024-07-31 10:22:47 +0530

    commit1

--- DailyLeetCodePrograms.java
+++ DailyLeetCodePrograms.java
@@ -150,6 +150,27 @@ class Solution {
         
     }
 }
+
+Question_Number:6) Water Bottles
+Input: Input: numBottles = 9, numExchange = 3
+Output: 13
+Explanation: You can exchange 3 empty bottles to get 1 full water bottle.
+Number of water bottles you can drink: 9 + 3 + 1 = 13.
+
+Solution:
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int drank = numBottles;
+        while(numBottles >= numExchange){
+            int exchanged = numBottles/numExchange;
+            int extraEmpty = numBottles%numExchange;
+            drank+=exchanged;
+            numBottles = exchanged + extraEmpty;
+        }
+        return drank;
+        
+    }
+}
 */
   
 }
